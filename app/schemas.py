b6056a29fff31d95a9db9551b68af945973d0bb9
Saraswathi-Kalirajan <saraswathik729@gmail.com>
2025-06-03 00:50:20 +0530

    check k

--- app/schemas.py
+++ app/schemas.py
@@ -1,147 +1,53 @@
-# # from pydantic import BaseModel, EmailStr, Field
-# # from datetime import datetime 
-# # from typing import Optional, Annotated
-# # from pydantic.types import conint
-# # class PostBase(BaseModel):
-# #     title: str
-# #     content:str
-# #     published: bool = True
-# #     #rating: Optional[int] = None 
-# # class PostCreate(PostBase):
-# #     pass
-
-# # class UserOut(BaseModel):
-# #     email: EmailStr 
-# #     id: int 
-# #     created_at: datetime
-# #     class Config:
-# #         orm_mode = True  
-
-# # class Post(PostBase):
-# #     id: int
-# #     created_at: datetime
-# #     owner_id: int
-# #     owner: UserOut
-# #     class Config:
-# #         orm_mode = True 
-
-# # class PostOut(BaseModel):
-# #     Post: Post 
-# #     votes: int
-# #     class Config:
-# #         orm_mode = True 
-
-# # class UserCreate(BaseModel):
-# #     email: EmailStr 
-# #     password: str 
-
-# # # class UserOut(BaseModel):
-# # #     email: EmailStr 
-# # #     id: int 
-# # #     created_at: datetime
-# # #     class Config:
-# # #         orm_mode = True 
-# # class UserLogin(BaseModel):
-# #     email: EmailStr
-# #     password: str
-
-# # class Token(BaseModel):
-# #     access_token: str 
-# #     token_type: str 
-
-# # class TokenData(BaseModel):
-# #     id: Optional[int] = None
-
-
-# # class Vote(BaseModel):
-# #     post_id: int
-# #     dir: Annotated[int, Field(le=1)]   
-
-
-# from pydantic import BaseModel, EmailStr
-# from datetime import datetime
-# from typing import Optional
-
+# from pydantic import BaseModel, EmailStr, Field
+# from datetime import datetime 
+# from typing import Optional, Annotated
 # from pydantic.types import conint
-
-# from app.models import User
-
-
 # class PostBase(BaseModel):
 #     title: str
-#     content: str
+#     content:str
 #     published: bool = True
-
-
+#     #rating: Optional[int] = None 
 # class PostCreate(PostBase):
 #     pass
 
-
 # class UserOut(BaseModel):
-#     id: int
-#     email: EmailStr
+#     email: EmailStr 
+#     id: int 
 #     created_at: datetime
-
 #     class Config:
-#         orm_mode = True
-
+#         orm_mode = True  
 
 # class Post(PostBase):
 #     id: int
 #     created_at: datetime
 #     owner_id: int
 #     owner: UserOut
-
 #     class Config:
-#         orm_mode = True
-
-
-# # class PostOut(BaseModel):
-# #     Post: Post
-# #     votes: int
-
-# #     class Config:
-# #         orm_mode = True  
-
+#         orm_mode = True 
 
 # class PostOut(BaseModel):
-#     id: int
-#     title: str
-#     content: str
-#     created_at: str
-#     owner: Optional[User] = None
+#     Post: Post 
 #     votes: int
-
 #     class Config:
-#         orm_mode = True
-
-
-# # class PostOut(BaseModel):
-# #     id: int
-# #     title: str
-# #     content: str
-# #     created_at: str
-# #     owner: Optional[UserOut] = None
-# #     votes: int
-
-# #     class Config:
-# #         orm_mode = True
-
+#         orm_mode = True 
 
 # class UserCreate(BaseModel):
-#     email: EmailStr
-#     password: str
+#     email: EmailStr 
+#     password: str 
 
-
+# # class UserOut(BaseModel):
+# #     email: EmailStr 
+# #     id: int 
+# #     created_at: datetime
+# #     class Config:
+# #         orm_mode = True 
 # class UserLogin(BaseModel):
 #     email: EmailStr
 #     password: str
 
-
 # class Token(BaseModel):
-#     access_token: str
-#     token_type: str
-
+#     access_token: str 
+#     token_type: str 
 
 # class TokenData(BaseModel):
 #     id: Optional[int] = None
@@ -149,9 +55,7 @@
 
 # class Vote(BaseModel):
 #     post_id: int
-#     dir: conint(le=1) 
-
-
+#     dir: Annotated[int, Field(le=1)]   
 
 
 from pydantic import BaseModel, EmailStr
@@ -159,6 +63,8 @@ from datetime import datetime
 from typing import Optional
 
 from pydantic.types import conint
+
+from app.models import User
 
 
 class PostBase(BaseModel):
@@ -190,12 +96,36 @@ class Post(PostBase):
         orm_mode = True
 
 
+# class PostOut(BaseModel):
+#     Post: Post
+#     votes: int
+
+#     class Config:
+#         orm_mode = True  
+
+
 class PostOut(BaseModel):
-    Post: Post
+    id: int
+    title: str
+    content: str
+    created_at: str
+    owner: Optional[User] = None
     votes: int
 
     class Config:
         orm_mode = True
+
+
+# class PostOut(BaseModel):
+#     id: int
+#     title: str
+#     content: str
+#     created_at: str
+#     owner: Optional[UserOut] = None
+#     votes: int
+
+#     class Config:
+#         orm_mode = True
 
 
 class UserCreate(BaseModel):
@@ -214,7 +144,7 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
 
 
 class Vote(BaseModel):
